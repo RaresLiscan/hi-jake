e16c8cade2af84543e8c8e30c2c51231aa434097
Rares Liscan <liscanrares@yahoo.com>
2022-06-24 01:53:09 +0300

    Added description for level 1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Level 1
+During this level, you will create the `Login` page.
+
+## Note: Before starting the process, change the `API_URL` from `auth.service.js` to your local IPv4 address
+
+Steps
+1. Open `Register.js`
+2. Add 3 text fields: email, password, confirmPassword and style them as you please
+3. Call the `register` function from the auth service and check if your account is being created
+4. After the user logged in, navigate them to the login page
--- app-expo/src/views/Login/Login.js
+++ app-expo/src/views/Login/Login.js
@@ -8,67 +8,18 @@ const Login = ({ navigation }) => {
     const [password, setPassword] = useState("");
 
     const loginUser = () => {
-        login(email, password)
-            .then(response => {
-                navigation.navigate("Homepage");
-            })
-            .catch(error => console.error(error));
+
     }
 
     return (
         <SafeAreaView>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Email..."
-                    placeholderTextColor={"#8f8f8f"}
-                    style={styles.input}
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                    keyboardType="email-address"
-                />
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Password..."
-                    placeholderTextColor={"#8f8f8f"}
-                    style={styles.input}
-                    onChangeText={(text) => setPassword(text)}
-                    value={password}
-                    textContentType='password'
-                    secureTextEntry={true}
-                />
-            </View>
-            <View style={styles.button}>
-                <Button title="Login" color={'green'} onPress={() => loginUser()} />
-            </View>
-            <View style={styles.button}>
-                <Button title="No account? Create one here" color={'gray'} onPress={() => navigation.navigate("Register")} />
-            </View>
+
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    input: {
-        height: 40,
-        margin: 10,
-        padding: 5,
-        borderWidth: 1,
-        color: 'black',
-        flex: 5,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        borderColor: '#000',
-        paddingBottom: 10,
-    },
-    icon: {
-        flex: 1,
-    },
-    button: {
-        margin: '3%',
-        justifyContent: 'center',
-    }
+
 });
 
 export default Login;
